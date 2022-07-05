aafea5bddd230f026927d798b2926a26f6a199ae
mahmudul hasan <mahmudulhasan@mahmuduls-MacBook-Pro.local>
2022-07-06 01:27:11 +0200

    Menu bar

---
+++ src/Page_1.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Page_1 = () => {
+  return (
+    <>
+<nav>
+    
+<div className='menu_container'>
+<div className='company_logo'><h1>Fitness Power</h1> </div>
+    <div className='menu_items'><li>HOME</li>
+                                <li>ABOUT</li>
+                                <li>SERVICE</li>
+                                <li>CONTACT</li></div>
+
+                                </div>
+    </nav>
+    </>
+  )
+}
+
+export default Page_1
